a7adab4a64c181dfdd6848f3c53b3a767a535546
ericag1985 <erica.gugliemella@gmail.com>
2016-04-24 19:28:12 -0400

    Update BasePage.java

--- src/main/java/com/PROJECT/en/BasePage.java
+++ src/main/java/com/PROJECT/en/BasePage.java
@@ -21,13 +21,43 @@ public class BasePage {
 
   public BasePage(WebDriver driver) {
     this.driver = driver;
-    wait = new WebDriverWait(driver, 5); //default wait
+    wait = new WebDriverWait(driver, 10); //default wait
   }
 
   //If you need a longer time from default wait
   public WebDriverWait waitFor(WebDriver driver, int seconds){
     WebDriverWait wait = new WebDriverWait(driver, seconds);
     return wait;
+  }
+
+  //Back Button action
+  public void backButton() { driver.navigate().back(); }
+
+  //Alert Dismiss actions
+  public void dismissAlertBox() {
+    JavascriptExecutor js = (JavascriptExecutor)driver;
+    js.executeScript( "window.onbeforeunload = function(e){};" );
+  }
+
+  //Switches to a new opened window
+  public void switchToNewWindow(WebElement element) {
+    for(String windowHandle : driver.getWindowHandles()){
+      driver.switchTo().window(windowHandle);
+    }
+    wait.until(ExpectedConditions.visibilityOf(element));
+  }
+
+  //Switch back to main window
+  public void switchToMainWindow(String element) {
+    driver.close();
+    driver.switchTo().window(element);
+  }
+
+  //Scroll into view to get around fixed headers
+  public void scrollIntoView(WebElement element) {
+    Point point = element.getLocation();
+    JavascriptExecutor js = (JavascriptExecutor)driver;
+    js.executeScript("window.scrollTo(0, "+ (point.getY() - 135) +")");
   }
 
   //Page load
@@ -50,33 +80,40 @@ public class BasePage {
 
   //Interaction with elements functions
   public void clickElement(WebElement element) {
+    wait.until(ExpectedConditions.elementToBeClickable(element));
     element.click();
   }
 
   public void hoverElement(WebElement element) {
+    wait.until(ExpectedConditions.visibilityOf(element));
     //Enables actions in browser
     Actions action = new Actions(driver);
-    //Hover action
     action.moveToElement(element).build().perform();
   }
 
+  public void slideElement(WebElement element) {
+    Actions action = new Actions(driver);
+    action.dragAndDropBy(element, -100, 0).build().perform();
+  }
+
   public void setElementText(WebElement element, String text) {
+    wait.until(ExpectedConditions.elementToBeClickable(element));
     //Clears field if text is already present
     element.clear();
-    //Send text
     element.sendKeys(text);
   }
 
   public void selectDropdownValue(WebElement element, String value) {
+    wait.until(ExpectedConditions.elementToBeClickable(element));
     //Finds select box
     Select select = new Select(element);
-    //Sets value
     select.selectByVisibleText(value);
   }
 
+  //If an element can't be clicked because "something else is in its place" (such as a hidden drop down menu), use a JS click instead of the regular click function.
   public void jsClick(WebElement element) {
+    wait.until(ExpectedConditions.elementToBeClickable(element));
     JavascriptExecutor js = (JavascriptExecutor)driver;
     js.executeScript("arguments[0].click();", element);
   }
-
 }
